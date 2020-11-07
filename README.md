12fa3374c9af77fb4f92de0cc2c8df80a52ff441
JadeP <jnporter.jp@gmail.com>
2020-11-06 22:26:25 -0800

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # API-Popquiz
+
+## Table of Contents
+
+    * Description
+
+    * Installation
+
+    * Usage
+
+### Description
+
+The client requested a multiple choice quiz that populates after clicking start.  Which includes a timer, score, and when a question is answered incorrectly time is subtracted.  A FINISHED/TIMES UP alert displays once the quiz is complete.  The highest scores are saved along with the user's information.
+
+## Installation
+
+No installations are required in order to access the quiz.  
+
+### Usage
+
+Unfortunately the code doesn't loop through the array, there is no timer, the score doesn't accumulate or populate, and also the prompts don't appear once a question is answered.
--- script.js
+++ script.js
@@ -1,40 +1,105 @@
 //listing variables//
 var multiquest = document.querySelector("#card-text");
 var next = document.querySelector("#nextBtn");
-var butA = document.querySelector("#buttonA");
-var butB = document.querySelector("#buttonB");
-var butC = document.querySelector("#buttonC");
-var answers = 
-var correctCounter = 0
-var incorrectCounter = 0
-
-//prompts//
-multiquest = questions
-var questions = [
-    prompt("1,2,3,4 are examples of which datatype? A:  String B: Boolean C: Number"),
-    prompt("Methods are functions tied to an?  A: Index B: Object C: Alert"),
-    prompt("What is the first index value of an array?  A: Index 0 B: Index 2 C: Index 3"),
-    prompt("What is a DOM?  A: Dominant Object Modal B: Document Object Model C: Document Objective Modualarity"),
-    prompt("Which equal sign's only use is to assign value?  A: ==  B: ===  C: =")
-];
+var butA = document.querySelector("#button-A");
+var butB = document.querySelector("#button-B");
+var butC = document.querySelector("#button-C");
+var options = document.querySelector(".buttons1");
+var scoreCounter = 0;
+var startBtn = document.querySelector("#button-start");
+var countDownTime = document.querySelector(".time");
 
 //conditional statements//
 
+//function start quiz
+function startQuiz() {
+    var questions = [
+        {
+            questionText : "1,2,3,4 are examples of which datatype?",
+            choices : ["A:  String", "B: Boolean", "C: Number"],
+            answer: "C: Number"
+        },
+        {
+            questionText : "Methods are functions tied to an?",
+            choices : ["A: Index", "B: Object", "C: Alert"],
+            answer: "B: Object"
+        },
+        {
+            questionText : "What is the first index value of an array?",
+            choices : ["A: Index 0", "B: Index 2", "C: Index 3"],
+            answer: "A: Index 0"
+        },
+        {
+            questionText : "What is the DOM?",
+            choices : ["A: Dominant Object Modal", "B: Document Object Model", "C: Document Objective Modualarity"],
+            answer: "B: Document Object Model"
+        },
+        {
+            questionText : "Which equal sign's only use is to assign value?",
+            choices : ["A: == ", "B: === ", "C: = "],
+            answer: "C: = "
+        }
+    ]
+    // start the timer
+    function timeFunc() {
+        setTimeout (myTimeout, 60000)
+    }
+    function myTimeout() {
+      countDownTime = (3000)
+    }
 
-//for loop//
-for (var i = 0; i < questions.length; i++) {
-    var response = window.prompt(questions[i].prompt)
-    if (response === questions[i].answers) {
-        correctCounter++
-        alert("great answer!")
+    // display first question, with possible choices
+    console.log("questions", questions)
+    console.log("questionText", questions[0].questionText)
+    multiquest.textContent = questions[0].questionText
+    butA.textContent = questions[0].choices[0];
+    butB.textContent = questions[0].choices[1];
+    butC.textContent = questions[0].choices[2];
+    multiquest.textContent = questions[1].questionText
+    butA.textContent = questions[1].choices[0];
+    butB.textContent = questions[1].choices[1];
+    butC.textContent = questions[1].choices[2];
+    multiquest.textContent = questions[2].questionText
+    butA.textContent = questions[2].choices[0];
+    butB.textContent = questions[2].choices[1];
+    butC.textContent = questions[2].choices[2];
+    multiquest.textContent = questions[3].questionText
+    butA.textContent = questions[3].choices[0];
+    butB.textContent = questions[3].choices[1];
+    butC.textContent = questions[3].choices[2];
+    multiquest.textContent = questions[4].questionText
+    butA.textContent = questions[4].choices[0];
+    butB.textContent = questions[4].choices[1];
+    butC.textContent = questions[4].choices[2];
+    
+    // and when you click on a choice
+    for (var i = 0; i < questions.length; i++) {
     }
-    else {incorrectCounter--
-        alert("wrong answer!")
-    }
+
+    // check if choice is correct answer or not, add of subtract from score
+    butA.onclick = console.log(butA.textContent);
+    butB.onclick = console.log(butB.textContent);
+    butC.onclick = console.log(butC.textContent);
+    // then go to next question, When out of questions or out of time, quiz stops
+    // question[i +1]
 }
 
+//for loop
+//for (var i = 0; i < questions.length; i++) {
+    //var response = questions[i].answer
+    //    if (response === questions[i].answer) {
+    //    scoreCounter++
+    //    alert("great answer!")
+    //    }
+    //    else alert("wrong answer!")
+     //   }
 
-next.addEventListener("click", function(event) {
+
+startBtn.onclick = startQuiz()
+startBtn.onclick = timeFunc()
+options.onclick = localStorage.choices
+next.onclick = [questions]
+
+// next.addEventListener("click", function(event) {
     
-  }
-});
+
